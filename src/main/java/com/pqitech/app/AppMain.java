57afdeaa6be984b2713eeffd704db17ce594afd7
dushibaiyu <dushibaiyu@foxmail.com>
2020-03-18 15:43:00 +0800

    log start

--- src/main/java/com/pqitech/app/AppMain.java
+++ src/main/java/com/pqitech/app/AppMain.java
@@ -3,5 +3,6 @@ package com.pqitech.app;
 public class AppMain {
     public static void main(String[] args) {
 
+
     }
 }
--- src/main/kotlin/com/pqitech/app/options.kt
+++ src/main/kotlin/com/pqitech/app/options.kt
@@ -1,17 +1,29 @@
 package com.pqitech.app
 
+import io.netty.util.internal.logging.InternalLoggerFactory
+import io.netty.util.internal.logging.Log4J2LoggerFactory
 import io.vertx.core.cli.CLI
 import io.vertx.core.cli.Option;
 import io.vertx.core.json.JsonObject;
+import org.apache.logging.log4j.core.config.ConfigurationSource
+import org.apache.logging.log4j.core.config.Configurator
+import java.io.FileInputStream
 import java.io.FileReader
 
 fun parseArgs(args : Array<String>,name : String = args[0]) : JsonObject
 {
-    val config = Option()
-    config.setShortName("c").setLongName("config").setDefaultValue("config.json").setDescription("设置程序的参数")
-    val cli = CLI.create("name").addOption(config).parse(args.toList())
-    val configFile = cli.getRawValueForOption(config)
-    println("the config file is: $configFile")
-    val file = FileReader(configFile)
-    return JsonObject(file.readText())
+  val config = Option()
+  config.setShortName("c").setLongName("config").setDefaultValue("config.json").setDescription("设置程序的参数")
+  val log = Option().setShortName("l").setLongName("log").setDefaultValue("").setDefaultValue("设置日志配置文件")
+  val cli = CLI.create(name).addOption(config).addOption(log).parse(args.toList())
+  val configFile = cli.getRawValueForOption(config)
+  println("the config file is: $configFile")
+  val logFilePath = cli.getRawValueForOption(log)
+  if(logFilePath.isNotEmpty()){
+    val fileLog = FileInputStream(logFilePath)
+    Configurator.initialize(null,ConfigurationSource(fileLog))
+  }
+  val file = FileReader(configFile)
+  InternalLoggerFactory.setDefaultFactory(Log4J2LoggerFactory.INSTANCE)
+  return JsonObject(file.readText())
 }
