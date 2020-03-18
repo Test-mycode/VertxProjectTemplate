c3d1021675fa01a0b557ad7d07fec19a1212a11d
dushibaiyu <dushibaiyu@foxmail.com>
2020-03-18 20:27:54 +0800

    config and options

--- src/main/kotlin/com/pqitech/app/options.kt
+++ src/main/kotlin/com/pqitech/app/options.kt
@@ -9,13 +9,13 @@ fun parseArgs(args : Array<String>,name : String = args[0]) : JsonObject
 {
   val config = Option()
   config.setShortName("c").setLongName("config").setDefaultValue("config.json").setDescription("设置程序的参数")
-  val log = Option().setShortName("l").setLongName("log").setDefaultValue("").setDefaultValue("设置日志配置文件")
+  val log = Option().setShortName("l").setLongName("log").setDefaultValue("log4j.xml").setDescription("设置日志配置文件")
   val cli = CLI.create(name).addOption(config).addOption(log).parse(args.toList())
   val configFile = cli.getRawValueForOption(config)
   println("the config file is: $configFile")
   val logFilePath = cli.getRawValueForOption(log)
   if(logFilePath.isNotEmpty()){
-    System.setProperty("log4j.configurationFile",logFilePath)
+    System.setProperty("log4j2.configurationFile",logFilePath)
   }
   val file = FileReader(configFile)
   return JsonObject(file.readText())
